d816ba010642fe817851999924be675ea949123f
Javier Chacón <j.chacon.alvarez@gmail.com>
2019-03-06 18:13:30 +0100

    Basic header/navigation frameworks added, basic app routes added

---
+++ src/shared/components/styledLink.js
@@ -0,0 +1,10 @@
+// Dependencies
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #FFF;
+`
+
+export default StyledLink;
