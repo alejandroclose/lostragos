b215e2f2c7a35110aafda64c06b9cf212cb90478
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2019-03-30 03:31:21 +0100

    added box-shaddow to gird cards

--- src/pages/home/grid/grid-card/grid-card.style.js
+++ src/pages/home/grid/grid-card/grid-card.style.js
@@ -3,23 +3,35 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  margin: 10px 10px 40px 10px;
+  max-width: 250px !important;
+  margin: 5px 0 20px 0;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
   border: 3px solid #D02936;
+
+  &:hover {
+
+    font-weight: bold;
+    box-shadow: 0px 0px 5px 0.5px #D02936;
 `
 export const Image = styled.div`
-  width: 250px;
-  height: 200px;
+  width: 200px;
+  height: 150px;
   background-image: url('${props => props.src}');
   background-repeat: no-repeat;
-  background-size: 100px 100px;
+  background-size: 80px 80px;
   background-position: center;
+  }
 
 `
 
 export const Text = styled.div`
   height: 50px;
   color: #D02936;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  }
 `
--- src/pages/home/grid/grid.style.js
+++ src/pages/home/grid/grid.style.js
@@ -13,7 +13,7 @@ export const GridWrapper = styled.div`
 
 export const GridCards = styled.div`
   width: 100%;
-  margin: 10px;
+  margin: 3px;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
