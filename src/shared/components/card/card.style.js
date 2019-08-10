320cd843d4545d34649a55840c75babf2b5a78bf
Brais Vazquez <brais.vazquez@netcentric.biz>
2019-08-10 20:30:40 +0200

    Card styling

--- src/shared/components/card/card.style.js
+++ src/shared/components/card/card.style.js
@@ -3,18 +3,37 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  max-width: 250px !important;
-  margin: 5px 0 20px 0;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  border: 2px solid #D02936;
+  
+  &.picture-box {
+  
+  }
 
-  &:hover {
-
-    font-weight: bold;
-    box-shadow: 0px 0px 1px 0.3px #D02936;
-    filter: drop-shadow(0px 0px 0.5px #D02936);
+  &.red-box {
+    max-width: 250px !important;
+    margin: 5px 0 20px 0;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    border: 2px solid #D02936;
+      
+    span {
+        height: 50px;
+        color: #D02936;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    
+    
+    &:hover {
+        font-weight: bold;
+        box-shadow: 0px 0px 1px 0.3px #D02936;
+        filter: drop-shadow(0px 0px 0.5px #D02936);
+    }
+  }
+  
 `
 export const Image = styled.div`
   width: 200px;
@@ -23,16 +42,6 @@ export const Image = styled.div`
   background-repeat: no-repeat;
   background-size: 80px 80px;
   background-position: center;
-  }
-
 `
 
-export const Text = styled.div`
-  height: 50px;
-  color: #D02936;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  }
-`
+export const Text = styled.span``
