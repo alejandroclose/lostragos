afafe19f49c07f6a0d0af70705c650b52390716e
Brais Vazquez <brais.vazquez@netcentric.biz>
2019-08-18 17:48:49 +0200

    Renaming card to cardOld

---
+++ src/shared/components/cardOld/cardOld.jsx
@@ -0,0 +1,27 @@
+// Dependencies
+import React, { useState } from 'react';
+
+import { Wrapper, Image, Icon, Text, Title } from './cardOld.style'
+
+export const CardOld = (props) => {
+    let image = props.icon;
+    let description = props.description;
+
+    if(props.seo === true) {
+        image = <img src={ image } alt={props.title}/>;
+    } else {
+        image = <Icon src={ image }/>;
+    }
+
+    if(props.description != false) description = <Text>{props.description}</Text>;
+
+    return (
+        <Wrapper className={props.theme} id={props.id}>
+            { image }
+            <Title>
+                {props.title}
+            </Title>
+            { description }
+        </Wrapper>
+    );
+};
---
+++ src/shared/components/cardOld/cardOld.style.js
@@ -0,0 +1,58 @@
+// Dependencies
+
+import styled from 'styled-components'
+
+export const Wrapper = styled.article`
+  
+  &.slider-box {
+    margin: 0 auto;
+    max-width: 25vw;
+  }
+    
+  &.red-box {
+    max-width: 250px !important;
+    margin: 5px 0 20px 0;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    border: 2px solid #D02936;
+      
+    header {
+        height: 50px;
+        color: #D02936;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    
+    
+    &:hover {
+        font-weight: bold;
+        box-shadow: 0px 0px 1px 0.3px #D02936;
+        filter: drop-shadow(0px 0px 0.5px #D02936);
+    }
+  }
+  
+`
+export const Image = styled.img`
+  width: 200px;
+  height: 150px;
+  background-image: url('${props => props.src}');
+  background-repeat: no-repeat;
+  background-size: 80px 80px;
+  background-position: center;
+`
+
+export const Icon = styled.div`
+  width: 200px;
+  height: 150px;
+  background-image: url('${props => props.src}');
+  background-repeat: no-repeat;
+  background-size: 80px 80px;
+  background-position: center;
+`
+
+export const Title = styled.header``
+export const Text = styled.p``
